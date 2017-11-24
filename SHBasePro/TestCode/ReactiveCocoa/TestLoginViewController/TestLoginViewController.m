deb9c9e10018dd50c507f7a4ac50e74168fbdbdd
mac <shenghai@ios-renhaisheng.local>
2017-11-24 16:47:04 +0800

    hhhhhh

---
+++ SHBasePro/TestCode/ReactiveCocoa/TestLoginViewController/TestLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestLoginViewController.m
+//  SHBasePro
+//
+//  Created by shenghai on 2017/11/23.
+//  Copyright © 2017年 ren. All rights reserved.
+//
+
+#import "TestLoginViewController.h"
+
+@interface TestLoginViewController ()
+
+@end
+
+@implementation TestLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
